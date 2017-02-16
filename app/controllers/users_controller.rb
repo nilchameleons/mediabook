922b9a7379c26b4ed22073cca27899a449af78d4
Cash Nelson <nil.chameleons@gmail.com>
2017-02-16 11:14:14 -0500

    added posts function and timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,22 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def index
-    @users = User.all
+    case params[:people]
+    when "friends"
+      @users = current_user.active_friends
+    when "requests"
+      @users = current_user.pending_friend_requests_from
+    when "pending"
+      @users = current_user.pending_friend_requests_to
+    else
+      @users = User.where.not(id: current_user.id)
+    end
   end
-  
+
   def show
+    @post = Post.new
+    @posts = @user.posts.order('created_at DESC')
+    @activities = PublicActivity::Activity.where(owner_id: @user.id)
   end
 
   def set_user
